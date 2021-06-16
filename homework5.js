c9f0b8a2fc99f302041d9f71a404ed308beac18e
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2021-06-16 06:07:43 -0700

    Update homework5.js

--- homework5.js
+++ homework5.js
@@ -1,3 +1,7 @@
+// task1
+// Given an array of numbers. Write a function to separate odd and even numbers in different
+// arrays.
+
 function oddAndEven (arr){
     let oddArr = []; 
     let evenArr = [];
@@ -14,9 +18,13 @@ array.push(evenArr);
     return array;
 }
     
-let length = +prompt("Enter a number")
+let length =+prompt("Enter array's length")
 let arr = [];
 for (let i = 0; i < length ; i++){
-    arr.push(+prompt("Enter numbers")) 
+    arr.push(+prompt("Enter array's numbers")) 
 }
-oddAndEven(arr)
+oddAndEven(arr);
+
+// task2
+// Given a word and a list of possible anagrams, select the correct sublist.
+
