f079b8577330c4d5efd401582d312db1bc5feb9f
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2021-06-17 07:26:45 -0700

    Update homework5.js

--- homework5.js
+++ homework5.js
@@ -28,3 +28,27 @@ oddAndEven(arr);
 // task2
 // Given a word and a list of possible anagrams, select the correct sublist.
 
+function wordSelect( w, a) {
+    let result;
+    for(let i = 0; i < a.length; i++ ){
+        if(w.length === a[i].length){
+            for(let j = 0; j < a[i].length; j++){
+                for(let k=0; k < w.length; k++){
+                    if(a[i][j] === w[k]){ 
+                        result = a[i]; 
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return result
+}
+let word = prompt("Enter a word");
+let length = +prompt ("Enter a number")
+let arr = []; 
+for (let i = 0; i < length; i++){
+    arr.push(prompt("Enter anagrams"))
+}
+wordSelect(word,arr);
+
